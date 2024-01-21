6e0a1cebba197187a28eb67a43998389bc8df710
jiwonpg98 <jiwonpg98@gmail.com>
2024-01-21 13:13:12 +0900

    init: backend init

---
+++ backend/src/main/kotlin/kr/co/title/academy/AcademyApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.title.academy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AcademyApplication
+
+fun main(args: Array<String>) {
+    runApplication<AcademyApplication>(*args)
+}
