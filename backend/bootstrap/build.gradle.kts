79a5cab6dab4acfcd405b4fb241d587459150563
jiwonpg98 <jiwonpg98@gmail.com>
2024-02-04 15:26:35 +0900

    feat: api 모듈 추가 및 기초 코드 추가

--- backend/bootstrap/build.gradle.kts
+++ backend/bootstrap/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
+	implementation(project(":api"))
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
