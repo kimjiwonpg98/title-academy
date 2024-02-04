29c9c5fcdf54c44fd9e967a644463baff4b824dd
jiwonpg98 <jiwonpg98@gmail.com>
2024-02-04 14:20:48 +0900

    feat: bootstrap 모듈 추가

---
+++ backend/bootstrap/build.gradle.kts
@@ -0,0 +1,14 @@
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.getByName("bootJar") {
+	enabled = true
+}
+
+tasks.getByName("jar") {
+	enabled = false
+}
---
+++ backend/bootstrap/src/test/kotlin/kr/co/title/bootstrap/BootstrapApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.title.bootstrap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BootstrapApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
